d3f9a8a30cdf07659131a4d2498814814e2f8b93
nytrealty <44840674+nytrealty@users.noreply.github.com>
2018-11-07 21:36:00 +0700

    Add files via upload

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://faceseo.vn/bannerweb/hoc-seo-chuyen-sau.jpg' width='500px'/><br/>13 Kỹ thuật đẩy top vượt trội</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='https://bit.ly/2PICA48' target='_blank'><img src='https://4.bp.blogspot.com/-mHim_ViIaps/W-L3YTVAM6I/AAAAAAAAAAg/ZiywpXlIGHAUOfVG62O8Uc0P_sBsf3bpwCLcBGAs/s320/5bcb454ec35a1.jpg' width='500px'/><br/>Dự án Đất nền Millennia City</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
